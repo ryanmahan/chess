7b82963fe4aa5352c3fae3671ed0821de141b051
Ryan Mahan <ryanmahan97@gmail.com>
2021-02-07 11:49:26 -0500

    cleanup stats

--- run_game.py
+++ run_game.py
@@ -6,36 +6,30 @@ from alphabeta import alphabeta
 
 board = chess.Board();
 
-move = { "value": 0, "move": None}
-color = chess.WHITE;
+turn = lambda x: "White" if x else "Black";
 then = datetime.now()
 start = datetime.now();
-while board.is_game_over() is False:
+
+def stats(board):
   print("moves: ", board.fullmove_number)
-  print("turn: ", board.turn)
+  print("turn: ", turn(board.turn))
   print("time since last move: ", datetime.now() - then);
   print("time since start: ", datetime.now() - start)
   print("current score: ", move["value"])
   print(board)
   then = datetime.now()
+
+move = { "value": 0, "move": None}
+color = chess.WHITE;
+
+while board.is_game_over() is False:
+  stats()
   move = alphabeta(board, 3, np.inf * -1, np.inf, True, custom(False));
   assert(board.is_legal(move["move"]))
   board.push(move["move"]);
-  print("moves: ", board.fullmove_number)
-  print("turn: ", board.turn)
-  print("time since last move: ", datetime.now() - then);
-  print("time since start: ", datetime.now() - start)
-  print("current score: ", move["value"])
-  print(board)
-  color = not color;
+  stats()
   move = alphabeta(board, 4, np.inf * -1, np.inf, False, claude_shannon(False));
   assert(board.is_legal(move["move"]))
   board.push(move["move"]);
-  color = not color;
 
-print("moves: ", board.fullmove_number)
-print("time since last move: ", datetime.now() - then);
-print("time since start: ", datetime.now() - start)
-print("current score: ", move["value"])
-print(board)
-print(board.result())
+stats()
